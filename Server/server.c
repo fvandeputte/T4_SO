274c72b24c95d859b4b41e3431af2c7a939a135d
jmcomber <jmcomber1@gmail.com>
2018-05-31 17:18:57 -0400

    makes, paquetes 5 y 6

--- Server/server.c
+++ Server/server.c
@@ -87,16 +87,13 @@ int main(int argc, char const *argv[])
                 perror("accept");
                 exit(EXIT_FAILURE);
         }
-
-        if (n_jugadores < 2){
-            pthread_t thread_id;
-            sockets[n_jugadores] = new_socket;
-            int *i = malloc(sizeof(*i));
-            *i = new_socket;
-            pthread_create(&thread_id, NULL, manejar_conexion_y_nickname, i);
-            threads[n_jugadores] = thread_id;
-            n_jugadores ++;
-        }
+        pthread_t thread_id;
+        sockets[n_jugadores] = new_socket;
+        int *i = malloc(sizeof(*i));
+        *i = new_socket;
+        pthread_create(&thread_id, NULL, manejar_conexion_y_nickname, i);
+        threads[n_jugadores] = thread_id;
+        n_jugadores ++;
 
 
     }
@@ -108,7 +105,37 @@ int main(int argc, char const *argv[])
     printf("Nombre (fuera de los threads) : %s \n", nick_uno);
 
     /// Estan los 2 con nombre, tengo que cruzarlos y enviarlos
+    
+    // 1er paquete 5
+    unsigned char message1[22];
+    message1[0] = 5;
+    message1[1] = 20;
+    for (int i = 0; i < 20; i++)
+    {
+        message1[2+i] = nick_dos[i];
+    }
+    send(sockets[0], message1 , 22 * sizeof(unsigned char), 0);
+    
+    // 2do paquete 5
+    unsigned char message2[22];
+    message2[0] = 5;
+    message2[1] = 20;
+    for (int i = 0; i < 20; i++)
+    {
+        message2[2+i] = nick_uno[i];
+    }
+    send(sockets[1], message2 , 22 * sizeof(unsigned char), 0);
 
+    // Paquete 6
+    unsigned char msg[4];
+    msg[0] = 6;
+    msg[1] = 2;
+    msg[2] = 1000 / 256;
+    msg[3] = 1000 % 256;
+    send(sockets[0], message2 , 4 * sizeof(unsigned char), 0);
+    send(sockets[1], message2 , 4 * sizeof(unsigned char), 0);
+
+    // Paquete 7
 
     return 0;
 }
